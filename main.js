cb283d633793e54d107a8f24cd25ea380bd0a7af
randobrando1008 <brandongregory1998@gmail.com>
2021-09-16 11:12:22 -0400

    Addition of React

---
+++ main.js
@@ -0,0 +1,4 @@
+function weightConverter(valNum) {
+	document.getElementById("inputGrams").innerHTML = valNum / 0.0022046;
+	document.getElementById("inputPounds").innerHTML = valNum*0.0022046;
+  }

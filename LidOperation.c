4cbae61c8584993c567550af8c4965c0aa4ab486
treydillan <90795080+treydillan@users.noreply.github.com>
2021-11-02 10:44:30 -0400

    Update LidOperation.c

--- LidOperation.c
+++ LidOperation.c
@@ -22,14 +22,14 @@ void Open(void)
     while (1)
     {
         
-        if (k>=1)            //Change motor position every 2 periods (50 times)
+        if (k>=1)            //Change motor position every period (100 times)
         {
             OC1RS=OC1RS+12;  //Increase duty cycle for PWM to change position
             if (OC1RS>=2800) //Vertical position, 7% Duty Cycle (2800/40000)
             {
                 return 1;    //Stop once the lid is open
             }
-            k=0;             // Reset Increment once above 2 periods
+            k=0;             // Reset Increment every period
         }
       
     }
@@ -40,7 +40,7 @@ void Close(void)
     T3CON=0x8010;            //1:8 prescale timer
     PR3=40000;               //Timer Interrupt Compare value for T = 20ms
     
-    OC1R=OC1RS=2800;         //Horizontal position, 4% Duty Cycle (1600/40000)
+    OC1R=OC1RS=2800;         //Vertical position, 7% Duty Cycle (2800/40000)
     OC1CON=0x000E;           //Continue in Idle, Select Timer 3, PWM Mode 
     
     _T3IF=0;                 //Start with IF Cleared
@@ -49,14 +49,14 @@ void Close(void)
     while (1)
     {
         
-        if (k>=1)            //Change motor position every 2 periods (50 times)
+        if (k>=1)            //Change motor position every period (100 times)
         {
-            OC1RS=OC1RS-12;  //Increase duty cycle for PWM to change position
-            if (OC1RS<=1600) //Vertical position, 7% Duty Cycle (2800/40000)
+            OC1RS=OC1RS-12;  //Decrease duty cycle for PWM to change position
+            if (OC1RS<=1600) //Horizontal position, 4% Duty Cycle (1600/40000)
             {
-                return 1;    //Stop once the lid is open
+                return 1;    //Stop once the lid is closed
             }
-            k=0;             // Reset Increment once above 2 periods
+            k=0;             // Reset Increment every period
         }
       
     }

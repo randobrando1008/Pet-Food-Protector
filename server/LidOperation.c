3871bb927e2c036a2da68e574e8067d9a6af4cd7
randobrando1008 <brandongregory1998@gmail.com>
2021-11-10 14:41:04 -0500

    Adding in verification of parameters

--- server/LidOperation.c
+++ server/LidOperation.c
@@ -1,4 +1,5 @@
 #include "mcc_generated_files/system.h"
+#include <time.h>
 
 int k;
 
@@ -25,7 +26,7 @@ void Open(void)
         if (k>=1)            //Change motor position every 2 periods (50 times)
         {
             OC1RS=OC1RS+12;  //Increase duty cycle for PWM to change position
-            if (OC1RS>=2800) //Vertical position, 7% Duty Cycle (2800/40000)
+            if (OC1RS>=2900) //Vertical position, 7% Duty Cycle (2800/40000)
             {
                 return 1;    //Stop once the lid is open
             }
@@ -40,7 +41,7 @@ void Close(void)
     T3CON=0x8010;            //1:8 prescale timer
     PR3=40000;               //Timer Interrupt Compare value for T = 20ms
     
-    OC1R=OC1RS=2800;         //Horizontal position, 4% Duty Cycle (1600/40000)
+    OC1R=OC1RS=2900;         //Horizontal position, 4% Duty Cycle (1600/40000)
     OC1CON=0x000E;           //Continue in Idle, Select Timer 3, PWM Mode 
     
     _T3IF=0;                 //Start with IF Cleared
@@ -62,12 +63,54 @@ void Close(void)
     }
 }
 
+bool checkDogID(int ID)
+{
+    if(ID == 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool checkTime(time_t time)
+{
+    if(time == time(NULL))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool checkLidStatus()
+{
+    if(OC1RS == 1600)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 int main(void)
 {
     SYSTEM_Initialize();
     
-    Open();
-    Close();
+    if(checkDogID(1) && checkTime(time(NULL)) && checkLidStatus())
+    {
+        Open();
+    }
+    else
+    {
+        Close();
+    }
 
     return 1;
 }

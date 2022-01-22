67eb86283973f6787b39c2efe5d49104b8d81de7
David Owens <dco9@uakron.edu>
2022-01-22 14:39:34 -0500

    Move app to src folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useState} from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -17,98 +9,46 @@ import {
   useColorScheme,
   View,
   Button,
+  TextInput,
   Image,
-  Pressable
 } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { format } from "date-fns";
 
-function LogoTitle() {
-  return (
-    <Image
-      style={{ resizeMode:'contain', alignSelf: 'center', height: 70, width: 400, right: 17 }}
-      source={require('./happyDoggo.png')}
-    />
-  );
-}
+import HomeScreen from './pages/HomeScreen';
+import DetailsScreen from './pages/DetailsScreen';
 
-function HomeScreen({ navigation }) {
-  return (
+class App extends React.Component {
 
-    <View >
-      <Button
-        className="styleLoginBtn"
-        title="Sign In"
-        onPress={() => navigation.navigate('Details')}
-      />
-      <Button
-        className="createAccBtn"
-        title="Create an Account"
-        onPress={() => navigation.navigate('CreateNewAccount')}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
-
-function NewAccountScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+          />
+          <Stack.Screen 
+            name="Details" 
+            component={DetailsScreen} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const Stack = createNativeStackNavigator();
 
-function App() {
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
-        />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="CreateNewAccount" component={NewAccountScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
 const styles = StyleSheet.create({
-  styleLoginBtn: {
-    flex: 1,
-    color: "white",
-    backgroundColor: "#00A5FF",
-    alignItems: 'center',
-    justifyContent: "center",
-    height: 50,
-    width: 190,
-    borderRadius: 25,
-    margin: 2
-  },
-  createAccBtn: {
-    flex: 1,
-    color: "#00A5FF",
-    backgroundColor: "white",
-    borderColor: "#00A5FF",
-    alignItems: 'center',
-    justifyContent: "center",
-    height: 50,
-    width: 190,
-    borderRadius: 25,
-    margin: 2
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
